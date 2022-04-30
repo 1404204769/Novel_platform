35b0cc7f7a6a4080cd7806fea45c68ad3b89ae4c
ubuntu <LZC20000318@163.com>
2022-05-01 00:49:12 +0800

    接口联调 0.5 增加了用户上传新资源，用户申请更改资源的接口

--- controllers/User_Resource.cc
+++ controllers/User_Resource.cc
@@ -370,25 +370,15 @@ void Resource::SearchContent(const HttpRequestPtr &req,std::function<void (const
 
 }
 
-// 图书上传接口
-void Resource::Upload(const HttpRequestPtr &req, std::function<void(const HttpResponsePtr &)> &&callback) const
+void MyUpload(Json::Value &ReqVal,Json::Value &RespVal)
 {
-    Json::Value ReqVal, RespVal, ParaJson;
-    drogon::HttpResponsePtr Result;
+    Json::Value ParaJson;
     auto MyJsonPtr = app().getPlugin<MyJson>();
     auto MyBasePtr = app().getPlugin<MyBase>();
     auto MyDBSPtr = app().getPlugin<MyDBService>();
-    const unordered_map<string, string> umapPara = req->getParameters();
-    MyBasePtr->TRACELog("Resource::Upload::body" + string(req->getBody()), true);
-
+    MyBasePtr->INFO_Func("MyUpload", true, ReqVal);
     try
     {
-        // 读取Json数据
-        ReqVal = *req->getJsonObject();
-        MyBasePtr->DEBUGLog("ReqVal::" + ReqVal.toStyledString(), true);
-
-        RespVal["简介"] = "图书上传接口";
-        MyJsonPtr->UnMapToJson(ReqVal, umapPara, "Para");
 
         // 检查数据完整性
         {
@@ -497,8 +487,9 @@ void Resource::Upload(const HttpRequestPtr &req, std::function<void(const HttpRe
                     MyBasePtr->DEBUGLog("建立资源贴完成", true);
 
 
-                    Json::Value CommentJson;
-                    MyJsonPtr->UnMapToJson(CommentJson, umapPara, "Para");
+                    Json::Value CommentJson ;
+                    CommentJson["Para"]["User_ID"] = ParaJson["User_ID"];
+                    CommentJson["Para"]["Login_Status"] = ParaJson["Login_Status"];
                     // "Note_ID" : 0, // 帖子ID
                     // "Comment_Content" : {
                     //     "Content":"",//表示内容
@@ -568,24 +559,459 @@ void Resource::Upload(const HttpRequestPtr &req, std::function<void(const HttpRe
 
         MyBasePtr->DEBUGLog("RespVal::" + RespVal.toStyledString(), true);
 
-        Result = HttpResponse::newHttpJsonResponse(RespVal);
     }
     catch (Json::Value &RespVal)
     {
         MyBasePtr->TRACE_ERROR(RespVal["ErrorMsg"]);
-        Result = HttpResponse::newHttpJsonResponse(RespVal);
     }
     catch (const drogon::orm::DrogonDbException &e)
     {
         RespVal["ErrorMsg"].append(e.base().what());
         MyBasePtr->TRACE_ERROR(RespVal["ErrorMsg"]);
-        Result = HttpResponse::newHttpJsonResponse(RespVal);
     }
     catch (...)
     {
         RespVal["ErrorMsg"].append("Resource::Upload::Error");
         MyBasePtr->TRACE_ERROR(RespVal["ErrorMsg"]);
-        Result = HttpResponse::newHttpJsonResponse(RespVal);
+    }
+    
+    MyBasePtr->INFO_Func("MyUpload", false, RespVal);
+}
+
+/*
+Req:{
+    "Book_ID":0,
+    "Book_Author":"爱潜水的乌贼",//作者
+    "Book_Name":"诡秘之主", // 书名
+    "Upload_Type" : "",// 类型
+    "Chapter_List":[
+        {
+            "Vol_Num":0,
+            "Chapter_Num":0,
+            "Chapter_Title": "",
+            "Content_Array": [
+                ""
+            ]
+        }
+    ]
+}
+Resp:{
+    "Chapter_List":[
+        {
+            "Vol_Num":0,
+            "Chapter_Num":0,
+            "Result":true/false
+        }
+    ]
+}
+*/
+void MyUploadChapterList(Json::Value &ReqVal,Json::Value &RespVal)
+{
+    auto MyJsonPtr = app().getPlugin<MyJson>();
+    auto MyBasePtr = app().getPlugin<MyBase>();
+    auto MyDBSPtr = app().getPlugin<MyDBService>();
+    MyBasePtr->INFO_Func("MyUploadNewChapterList", true, ReqVal);
+    try
+    {
+
+        // 检查数据完整性
+        {
+            MyBasePtr->DEBUGLog("开始检查数据完整性", true);
+            // "Book_ID":0,
+            // "Book_Author":"爱潜水的乌贼",//作者
+            // "Book_Name":"诡秘之主", // 书名
+            // "Upload_Type" : "",// 类型
+            // "Chapter_List":[]
+            // 创建检查列表 key是字段名 value 是字段类型
+            std::map<string, MyJson::ColType> ColMap;
+            ColMap["Book_Author"] = MyJson::ColType::STRING;
+            ColMap["Book_Name"] = MyJson::ColType::STRING;
+            ColMap["Upload_Type"] = MyJson::ColType::STRING;
+            ColMap["Book_ID"] = MyJson::ColType::INT;
+            ColMap["Chapter_List"] = MyJson::ColType::ARRAY;
+            MyJsonPtr->checkMemberAndTypeInMap(ReqVal, RespVal, ColMap);
+            MyBasePtr->DEBUGLog("检查数据完整性完成", true);
+        }
+        // 先检查一遍章节内容，去掉数据不完整的
+        int chaptersize = ReqVal["Chapter_List"].size();
+        Json::Value chap,ChapterReq,ChapterResp;
+        Json::Value TempReq,TempResp;
+        
+        ChapterReq["Para"] = ReqVal["Para"];
+        ChapterReq["Book_ID"] = ReqVal["Book_ID"];
+        ChapterReq["Book_Author"] = ReqVal["Book_Author"];
+        ChapterReq["Book_Name"] = ReqVal["Book_Name"];
+        ChapterReq["Upload_Type"] = ReqVal["Upload_Type"];
+        for(int i = 0;i<chaptersize;i++)
+        {
+            TempReq.clear();
+            TempResp.clear();
+            ChapterResp.clear();
+            TempReq = ReqVal["Chapter_List"][i];
+            chap["Vol_Num"] = TempReq["Vol_Num"];
+            chap["Chapter_Num"] = TempReq["Chapter_Num"];
+            try
+            {
+                // 检查数据完整性
+                {
+                    MyBasePtr->DEBUGLog("开始检查数据完整性", true);
+                    // "Vol_Num":0,
+                    // "Chapter_Num":0,
+                    // "Chapter_Title": "",
+                    // "Content_Array": [""]
+                    // 创建检查列表 key是字段名 value 是字段类型
+                    std::map<string, MyJson::ColType> ColMap;
+                    ColMap["Vol_Num"] = MyJson::ColType::INT;
+                    ColMap["Chapter_Num"] = MyJson::ColType::INT;
+                    ColMap["Chapter_Title"] = MyJson::ColType::STRING;
+                    ColMap["Content_Array"] = MyJson::ColType::ARRAY;
+                    MyJsonPtr->checkMemberAndTypeInMap(TempReq, TempResp, ColMap);
+                    MyBasePtr->DEBUGLog("检查数据完整性完成", true);
+                }
+                chap["Result"] = true;
+            }
+            catch(Json::Value &e)
+            {
+                chap["Result"] = false;
+            }
+            // 如果存在缺陷，则跳过这个
+            if(!chap["Result"].asBool())
+            {
+                RespVal["Chapter_List"].append(chap);
+                continue;
+            }
+
+            ChapterReq["Content"] = TempReq;
+            MyUpload(ChapterReq,ChapterResp);
+            int uploadid = ChapterResp["Upload_ID"].asInt();
+            TempReq.clear();
+            TempResp.clear();
+            TempReq["Upload_ID"] = uploadid;
+            MyDBSPtr->Search_Upload_By_UploadID(TempReq,TempResp);
+            if(!TempResp["Result"].asBool())
+            {
+                chap["Result"] = false;
+            }
+            if(TempResp["Upload_Data"]["Status"].asString() == "system已拒绝")
+            {
+                chap["Result"] = false;
+            }
+            if(TempResp["Upload_Data"]["Status"].asString() == "system已允许/已成功")
+            {
+                chap["Result"] = true;
+            }
+
+            RespVal["Chapter_List"].append(chap);
+        }
+    }
+    catch (Json::Value &e)
+    {
+        MyBasePtr->TRACE_ERROR(e["ErrorMsg"]);
+        // 设置返回格式
+        int ErrorSize = e["ErrorMsg"].size();
+        RespVal["Result"] = false;
+        RespVal["Message"] = e["ErrorMsg"][ErrorSize - 1];
+
+    }
+    
+    MyBasePtr->INFO_Func("MyUploadNewChapterList", false, RespVal);
+}
+
+// 图书上传接口
+void Resource::Upload(const HttpRequestPtr &req, std::function<void(const HttpResponsePtr &)> &&callback) const
+{
+    Json::Value ReqVal, RespVal, ParaJson;
+    drogon::HttpResponsePtr Result;
+    auto MyJsonPtr = app().getPlugin<MyJson>();
+    auto MyBasePtr = app().getPlugin<MyBase>();
+    auto MyDBSPtr = app().getPlugin<MyDBService>();
+    const unordered_map<string, string> umapPara = req->getParameters();
+    MyBasePtr->TRACELog("Resource::Upload::body" + string(req->getBody()), true);
+
+    // 读取Json数据
+    ReqVal = *req->getJsonObject();
+    MyBasePtr->DEBUGLog("ReqVal::" + ReqVal.toStyledString(), true);
+
+    RespVal["简介"] = "图书上传接口";
+    MyJsonPtr->UnMapToJson(ReqVal, umapPara, "Para");
+    MyUpload(ReqVal,RespVal);
+    Result = HttpResponse::newHttpJsonResponse(RespVal);
+    Result->setStatusCode(k200OK);
+    Result->setContentTypeCode(CT_TEXT_HTML);
+    callback(Result);
+}
+
+void Resource::UploadLN(const HttpRequestPtr &req,std::function<void (const HttpResponsePtr &)> &&callback) const
+{
+    //"system已拒绝"
+    //"system已允许/已成功"
+    Json::Value ReqVal, RespVal, ParaJson,ResultData;
+    drogon::HttpResponsePtr Result;
+    auto MyJsonPtr = app().getPlugin<MyJson>();
+    auto MyBasePtr = app().getPlugin<MyBase>();
+    auto MyDBSPtr = app().getPlugin<MyDBService>();
+    const unordered_map<string, string> umapPara = req->getParameters();
+    MyBasePtr->TRACELog("Resource::UploadLN::body" + string(req->getBody()), true);
+
+    try
+    {
+        // 读取Json数据
+        ReqVal = *req->getJsonObject();
+        MyBasePtr->DEBUGLog("ReqVal::" + ReqVal.toStyledString(), true);
+
+        RespVal["简介"] = "图书上传列表新接口";
+        MyJsonPtr->UnMapToJson(ReqVal, umapPara, "Para");
+
+        // 检查数据完整性
+        {
+            MyBasePtr->DEBUGLog("开始检查数据完整性", true);
+            // "Book_Author":"爱潜水的乌贼",//作者
+            // "Book_Name":"诡秘之主", // 书名
+            // "Book_Publisher":"起点中文网",// 签约平台
+            // "Book_Synopsis":"" // 提要
+        
+            //  "Chapter_List":[],//章节列表
+            // 创建检查列表 key是字段名 value 是字段类型
+            std::map<string, MyJson::ColType> ColMap;
+            ColMap["Book_Author"] = MyJson::ColType::STRING;
+            ColMap["Book_Name"] = MyJson::ColType::STRING;
+            ColMap["Book_Publisher"] = MyJson::ColType::STRING;
+            ColMap["Book_Synopsis"] = MyJson::ColType::STRING;
+            ColMap["Chapter_List"] = MyJson::ColType::ARRAY;
+            MyJsonPtr->checkMemberAndTypeInMap(ReqVal, RespVal, ColMap);
+            MyBasePtr->DEBUGLog("检查数据完整性完成", true);
+        }
+
+        // 先判断此书是否存在
+        Json::Value TempReq,TempResp;
+        TempReq["Book_Name"] = ReqVal["Book_Name"];
+        TempReq["Author"] = ReqVal["Book_Author"];
+        TempReq["Publisher"] = ReqVal["Book_Publisher"];
+        MyDBSPtr->Search_Book(TempReq,TempResp);
+        int BookID = 0;
+        TempReq.clear();
+        MyJsonPtr->UnMapToJson(TempReq, umapPara, "Para");
+        TempReq["Book_ID"] = 0;
+        TempReq["Book_Author"] = ReqVal["Book_Author"];
+        TempReq["Book_Name"] = ReqVal["Book_Name"];
+        TempReq["Upload_Type"] = "new_book";
+        TempReq["Content"]["Book_Author"] = ReqVal["Book_Author"];
+        TempReq["Content"]["Book_Name"] = ReqVal["Book_Name"];
+        TempReq["Content"]["Book_Publisher"] = ReqVal["Book_Publisher"];
+        TempReq["Content"]["Book_Synopsis"] = ReqVal["Book_Synopsis"];
+
+        // 根据情况查询BookID
+        if(!TempResp["Result"].asBool())
+        {
+            // 不存在
+            RespVal["ErrorMsg"].append("目标图书不存在");
+            throw RespVal;
+        }
+        else{
+            // 判读是否唯一
+            if(TempResp["Book_List"].size() > 1)
+            {
+                // 不唯一
+                RespVal["ErrorMsg"].append("目标图书不唯一");
+                throw RespVal;
+            }
+            else if(TempResp["Book_List"].size() == 1)
+            {
+                BookID = TempResp["Book_List"][0]["Book_ID"].asInt();
+            }
+            else
+            {
+                TempResp.clear();
+                MyUpload(TempReq,TempResp);
+                int uploadid = TempResp["Upload_ID"].asInt();
+                TempReq.clear();
+                TempResp.clear();
+                TempReq["Upload_ID"] = uploadid;
+                MyDBSPtr->Search_Upload_By_UploadID(TempReq,TempResp);
+                if(!TempResp["Result"].asBool())
+                {
+                    RespVal["ErrorMsg"].append("新图书上传ID异常");
+                    throw RespVal;
+                }
+                if(TempResp["Upload_Data"]["Status"].asString() == "system已拒绝")
+                {
+                    RespVal["ErrorMsg"].append("新图书上传已被系统拒绝");
+                    throw RespVal;
+                }
+                if(TempResp["Upload_Data"]["Status"].asString() == "system已允许/已成功")
+                {
+                    BookID = TempResp["Upload_Data"]["Book_ID"].asInt();
+                }
+            }
+        }
+
+        ReqVal["Book_ID"] = BookID;
+        ReqVal["Upload_Type"] = "old_book_new";
+        MyUploadChapterList(ReqVal,RespVal);
+
+        // 设置返回格式
+        ResultData["Result"] = true;
+        ResultData["Message"] = "用户上传资源成功";
+        ResultData["Data"]["Chapter_List"] = RespVal["Chapter_List"];
+
+        Result = HttpResponse::newHttpJsonResponse(ResultData);
+    }
+    catch (Json::Value &RespVal)
+    {
+        MyBasePtr->TRACE_ERROR(RespVal["ErrorMsg"]);
+        // 设置返回格式
+        int ErrorSize = RespVal["ErrorMsg"].size();
+        ResultData["Result"] = false;
+        ResultData["Message"] = RespVal["ErrorMsg"][ErrorSize - 1];
+
+        Result = HttpResponse::newHttpJsonResponse(ResultData);
+    }
+    catch (const drogon::orm::DrogonDbException &e)
+    {
+        RespVal["ErrorMsg"].append(e.base().what());
+        MyBasePtr->TRACE_ERROR(RespVal["ErrorMsg"]);
+        // 设置返回格式
+        int ErrorSize = RespVal["ErrorMsg"].size();
+        ResultData["Result"] = false;
+        ResultData["Message"] = RespVal["ErrorMsg"][ErrorSize - 1];
+
+        Result = HttpResponse::newHttpJsonResponse(ResultData);
+    }
+    catch (...)
+    {
+        RespVal["ErrorMsg"].append("Resource::UploadLN::Error");
+        MyBasePtr->TRACE_ERROR(RespVal["ErrorMsg"]);
+        // 设置返回格式
+        int ErrorSize = RespVal["ErrorMsg"].size();
+        ResultData["Result"] = false;
+        ResultData["Message"] = RespVal["ErrorMsg"][ErrorSize - 1];
+
+        Result = HttpResponse::newHttpJsonResponse(ResultData);
+    }
+
+    Result->setStatusCode(k200OK);
+    Result->setContentTypeCode(CT_TEXT_HTML);
+    callback(Result);
+}
+
+void Resource::UploadLO(const HttpRequestPtr &req,std::function<void (const HttpResponsePtr &)> &&callback) const
+{
+
+    //"system已拒绝"
+    //"system已允许/已成功"
+    Json::Value ReqVal, RespVal, ParaJson,ResultData;
+    drogon::HttpResponsePtr Result;
+    auto MyJsonPtr = app().getPlugin<MyJson>();
+    auto MyBasePtr = app().getPlugin<MyBase>();
+    auto MyDBSPtr = app().getPlugin<MyDBService>();
+    const unordered_map<string, string> umapPara = req->getParameters();
+    MyBasePtr->TRACELog("Resource::UploadLO::body" + string(req->getBody()), true);
+
+    try
+    {
+        // 读取Json数据
+        ReqVal = *req->getJsonObject();
+        MyBasePtr->DEBUGLog("ReqVal::" + ReqVal.toStyledString(), true);
+
+        RespVal["简介"] = "图书上传列表新接口";
+        MyJsonPtr->UnMapToJson(ReqVal, umapPara, "Para");
+
+        // 检查数据完整性
+        {
+            MyBasePtr->DEBUGLog("开始检查数据完整性", true);
+            // "Book_Author":"爱潜水的乌贼",//作者
+            // "Book_Name":"诡秘之主", // 书名
+            // "Book_Publisher":"起点中文网",// 签约平台
+            //  "Chapter_List":[],//章节列表
+            // 创建检查列表 key是字段名 value 是字段类型
+            std::map<string, MyJson::ColType> ColMap;
+            ColMap["Book_Author"] = MyJson::ColType::STRING;
+            ColMap["Book_Name"] = MyJson::ColType::STRING;
+            ColMap["Book_Publisher"] = MyJson::ColType::STRING;
+            ColMap["Chapter_List"] = MyJson::ColType::ARRAY;
+            MyJsonPtr->checkMemberAndTypeInMap(ReqVal, RespVal, ColMap);
+            MyBasePtr->DEBUGLog("检查数据完整性完成", true);
+        }
+
+        // 先判断此书是否存在
+        Json::Value TempReq,TempResp;
+        TempReq["Book_Name"] = ReqVal["Book_Name"];
+        TempReq["Author"] = ReqVal["Book_Author"];
+        TempReq["Publisher"] = ReqVal["Book_Publisher"];
+        MyDBSPtr->Search_Book(TempReq,TempResp);
+        int BookID = 0;
+        TempReq.clear();
+        MyJsonPtr->UnMapToJson(TempReq, umapPara, "Para");
+
+        // 根据情况查询BookID
+        if(!TempResp["Result"].asBool())
+        {
+            // 不存在
+            RespVal["ErrorMsg"].append("目标图书不存在");
+            throw RespVal;
+        }
+        else{
+            // 判读是否唯一
+            if(TempResp["Book_List"].size() > 1)
+            {
+                // 不唯一
+                RespVal["ErrorMsg"].append("目标图书不唯一");
+                throw RespVal;
+            }
+            else if(TempResp["Book_List"].size() == 1)
+            {
+                BookID = TempResp["Book_List"][0]["Book_ID"].asInt();
+            }
+            else
+            {
+                // 不存在
+                RespVal["ErrorMsg"].append("目标图书不存在");
+                throw RespVal;
+            }
+        }
+
+        ReqVal["Book_ID"] = BookID;
+        ReqVal["Upload_Type"] = "old_book_old";
+        MyUploadChapterList(ReqVal,RespVal);
+        
+        // 设置返回格式
+        ResultData["Result"] = true;
+        ResultData["Message"] = "用户申请更改资源成功";
+        ResultData["Data"]["Chapter_List"] = RespVal["Chapter_List"];
+
+        Result = HttpResponse::newHttpJsonResponse(ResultData);
+    }
+    catch (Json::Value &RespVal)
+    {
+        MyBasePtr->TRACE_ERROR(RespVal["ErrorMsg"]);
+        // 设置返回格式
+        int ErrorSize = RespVal["ErrorMsg"].size();
+        ResultData["Result"] = false;
+        ResultData["Message"] = RespVal["ErrorMsg"][ErrorSize - 1];
+
+        Result = HttpResponse::newHttpJsonResponse(ResultData);
+    }
+    catch (const drogon::orm::DrogonDbException &e)
+    {
+        RespVal["ErrorMsg"].append(e.base().what());
+        MyBasePtr->TRACE_ERROR(RespVal["ErrorMsg"]);
+        // 设置返回格式
+        int ErrorSize = RespVal["ErrorMsg"].size();
+        ResultData["Result"] = false;
+        ResultData["Message"] = RespVal["ErrorMsg"][ErrorSize - 1];
+
+        Result = HttpResponse::newHttpJsonResponse(ResultData);
+    }
+    catch (...)
+    {
+        RespVal["ErrorMsg"].append("Resource::UploadLO::Error");
+        MyBasePtr->TRACE_ERROR(RespVal["ErrorMsg"]);
+        // 设置返回格式
+        int ErrorSize = RespVal["ErrorMsg"].size();
+        ResultData["Result"] = false;
+        ResultData["Message"] = RespVal["ErrorMsg"][ErrorSize - 1];
+
+        Result = HttpResponse::newHttpJsonResponse(ResultData);
     }
 
     Result->setStatusCode(k200OK);
